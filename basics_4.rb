b8e387e1239184c7777e6e4f0311594632eb594b
ashleyyi <ashleysolyi@gmail.com>
2014-10-07 21:06:39 +1300

    Precourse Basics exercise

---
+++ basics_4.rb
@@ -0,0 +1,8 @@
+dates = [1994, 1972, 2008, 1994]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+
+
---
+++ basics_7.rb
@@ -0,0 +1,2 @@
+# SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}' from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+# this means that Ruby was expecting to see } to close off an a hash but instead found ) closing an array\
